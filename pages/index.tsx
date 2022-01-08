ada014cc46fd8f159a5ac269430712a9abf846b5
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2022-01-08 15:41:33 +0100

    testing seo tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,26 @@ import { Container } from "@mantine/core";
 import { dehydrate, QueryClient } from "react-query";
 import Sections from "../components/common/Sections";
 import { novelsFetch, useNovels } from "../components/hooks/useNovels";
+import Head from "next/head";
 
+const siteName = process.env.NEXT_PUBLIC_SITE_NAME;
 export default function HomePage() {
   const { data } = useNovels();
-
+  const title = `${siteName} - Read Chinese, Korean and Japanese Novels`;
+  const description = `${siteName} has the latest translations of your favorite Chinese, Japanese, Korean - Light Novels and Web Novels. All Chapters Are Updated Daily and New Novels Added Very Frequently.`;
   return (
     <>
+      <Head>
+        <title>{`${siteName} - Read Chinese, Korean and Japanese Novels`}</title>
+        <meta name="description" content={description} />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={`${siteName}`} />
+        <meta name="twitter:label1" content="Est reading time" />
+        <meta name="twitter:data1" content="5 minutes" />
+      </Head>
+
       <Container>
         {data?.map((category: any) => (
           <Sections

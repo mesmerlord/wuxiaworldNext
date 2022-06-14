f66da232785d195b72fde3970478734700b28f7f
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-06-14 20:18:36 +0200

    checking

--- pages/chapter/[slug].jsx
+++ pages/chapter/[slug].jsx
@@ -41,18 +41,15 @@ export async function getStaticPaths() {
     const value = { slug: item.slug };
     return value;
   });
-  const chapter_urls = urls.map(async (novel) => {
-    const chapter_fetch = async (id) => {
-      return await axios
-        .get(`${apiHome}/chapters/${id}/`, {})
-        .then((response) => {
-          const res = response.data;
-          return res;
-        })
-        .catch((error) => console.log(error));
-    };
+  const chapter_urls = urls.map( (novel) => {
+    const fetched_chapters = await axios
+    .get(`${apiHome}/chapters/${id}/`, {})
+    .then((response) => {
+      const res = response.data;
+      return res;
+    })
+    .catch((error) => console.log(error));
 
-    const fetched_chapters = await chapter_fetch(novel.slug);
     console.log(fetched_chapters);
     const first_chaps_to_download = fetched_chapters.slice(0, 50);
     const second_chaps_to_download = fetched_chapters.slice(-50);
